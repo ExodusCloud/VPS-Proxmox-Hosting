90e76dfb7dc40381912d331ea74c2be2799d66f1
ExodusCloud <exoduscloud.co.id@gmail.com>
2024-10-10 09:18:08 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,26 +1,20 @@
 #!/bin/bash
+PROCESSED_FILE="processed.txt"  
+PERMISSION="PVEVMUser"
+DELAY=10
 
-# Konfigurasi
-PROCESSED_FILE="processed.txt"  # File untuk menyimpan container dan user yang sudah diproses
-PERMISSION="PVEVMUser"                    # Ganti dengan permission yang diinginkan
-DELAY=10                                   # Waktu tunda dalam detik sebelum memeriksa lagi
-
-# Membaca container dan user yang sudah diproses
 if [ ! -f "$PROCESSED_FILE" ]; then
   touch "$PROCESSED_FILE"
 fi
 
 while true; do
-  # Membaca daftar container
-  containers=$(pvesh get /nodes/www/lxc --output-format json | jq -r '.[].name')
+  containers=$(pvesh get /nodes/PROXMOX_NODE/lxc --output-format json | jq -r '.[].name')
 
-  # Membaca container yang sudah diproses
   PROCESSED_LIST=()
   while read -r line; do
     PROCESSED_LIST+=("$line")
   done < "$PROCESSED_FILE"
 
-  # Deteksi container baru
   for container in $containers; do
     user="${container}@pve"
     if [[ ! " ${PROCESSED_LIST[@]} " =~ " ${container} " ]]; then
@@ -32,7 +26,5 @@ while true; do
       fi
     fi
   done
-
-  # Tunggu sebelum memeriksa lagi
   sleep $DELAY
 done
